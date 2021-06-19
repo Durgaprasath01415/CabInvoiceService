51914f4b82f6253cbf81a60388f3c6e94bbdbb11
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-19 10:11:13 +0530

    ADDED : Checking of minimum fare and test case for it

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -11,4 +11,12 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25,fare,0.0);
     }
 
+    @Test
+    public void givenDistanceOrTime_ShouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
