67956fef8dbd3b1f8248ef7d500676c8bec21c23
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-22 10:04:15 +0530

    REFACTORED : Spacings between test case

--- src/main/java/InvoiceSummary.java
+++ src/main/java/InvoiceSummary.java
@@ -1,12 +1,12 @@
 public class InvoiceSummary {
-    private final int numOfRides;
+    private final int numberOfRides;
     private final double totalFare;
     private final double avgFare;
 
-    public InvoiceSummary(int numOfRides, double totalFare) {
-        this.numOfRides = numOfRides;
+    public InvoiceSummary(int numberOfRides, double totalFare) {
+        this.numberOfRides = numberOfRides;
         this.totalFare = totalFare;
-        this.avgFare = this.totalFare/this.numOfRides;
+        this.avgFare = this.totalFare/this.numberOfRides;
     }
 
     @Override
@@ -14,13 +14,13 @@ public class InvoiceSummary {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         InvoiceSummary that = (InvoiceSummary) o;
-        return numOfRides == that.numOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.avgFare, avgFare) == 0;
+        return numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.avgFare, avgFare) == 0;
     }
 
     @Override
     public String toString() {
         return "InvoiceSummary{" +
-                "numOfRides=" + numOfRides +
+                "numOfRides=" + numberOfRides +
                 ", totalFare=" + totalFare +
                 ", avgFare=" + avgFare +
                 '}';
